680796cf0b6c4af6d500563e10b1eb32e308c51c
malou <mael.demortier@eleves.enpc.fr>
2016-01-06 10:00:13 +0100

    Test de page web avec django, contenant une application polls.

---
+++ serveur/mysite/polls/models.py
@@ -0,0 +1,48 @@
+from django.db import models
+import datetime
+from django.utils import timezone
+
+# Create your models here.
+
+class Poll(models.Model):
+    question = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+    def __unicode__(self):
+        return self.question
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+class Choice(models.Model):
+    poll = models.ForeignKey(Poll)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+    def __unicode__(self):
+        return self.choice_text
+
+class Blog(models.Model):
+    name = models.CharField(max_length=100)
+    tagline = models.TextField()
+    def __unicode__(self):
+        return self.name
+
+class Author(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.EmailField()
+    def __unicode__(self):
+        return self.name
+
+class Entry(models.Model):
+    blog = models.ForeignKey(Blog)
+    headline = models.CharField(max_length=255)
+    body_text = models.TextField()
+    pub_date = models.DateField()
+    mod_date = models.DateField()
+    authors = models.ManyToManyField(Author)
+    n_comments = models.IntegerField()
+    n_pingbacks = models.IntegerField()
+    rating = models.IntegerField()
+    def __unicode__(self):
+        return self.headline
+
+
+
