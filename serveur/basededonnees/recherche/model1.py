01238fdb8da033457c2baab6db29d16a785bf55c
malou <mael.demortier@eleves.enpc.fr>
2016-01-13 13:32:55 +0100

    mise à jour de la base de donnée, version simplifiéé

--- serveur/basededonnees/recherche/model1.py
+++ serveur/basededonnees/recherche/model1.py
@@ -1,38 +1,40 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
 
-class Personne(models.Model):
-    nom = models.CharField(max_length=50)
-    prenom = models.CharField(max_length=50)
-    mdp = models.CharField(max_length=50)
-    #assos = models.ForeignKey('Asso')
-
-    def __str__(self):
-        return self.prenom
-
 class Asso(models.Model):
     nom = models.CharField(max_length=50)
-    team = models.ManyToManyField(Personne)
 
     def __str__(self):
         return self.nom
 
 class Materiel(models.Model):
     nom = models.CharField(max_length=50)
+    quantite = models.IntegerField()
     etat = models.BooleanField()
     asso = models.ForeignKey('Asso')
-    #pret = models.ManyToManyField(Personne, through='Pret')
 
     def __str__(self):
         return self.nom
 
+class Personne(models.Model):
+    nom = models.CharField(max_length=50)
+    prenom = models.CharField(max_length=50)
+    identifiant = models.CharField(max_length=50)
+    mdp = models.CharField(max_length=50)
+    objets = models.ManyToManyField(Materiel, through='Pret')
+
+    def __str__(self):
+        return self.identifiant
+
 class Pret(models.Model):
     date_pret = models.DateTimeField(auto_now_add=False, auto_now=True, verbose_name="Date de Pret")
     #date_retour = models.DateTimeField(default=datetime.date.today(), auto_now=False, verbose_name="Date de Retour")
-    materiel = models.ForeignKey('Materiel')
-    #preteur = models.ForeignKey('Personne')
-    emprunteur = models.ForeignKey('Personne')
+    materiel = models.ForeignKey(Materiel)
+    preteur = models.ForeignKey(Personne)
+    #emprunteur = models.ForeignKey(Personne)
+    #assos?
 
     def __str__(self):
         return "{0} prete par {1} a {2}".format(self.materiel, self.preteur, self.emprunteur)
